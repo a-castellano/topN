32e308669146ea20a04d517af5a35c4af919939d
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2019-07-20 09:32:10 +0200

    Min heap works

--- topN.go
+++ topN.go
@@ -2,13 +2,35 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
 )
 
-func getResults(filename string, numberOfResults int) {
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func getResults(filename string, numberOfResults int, minHeap *IntHeap) {
+
 	fmt.Println(filename)
 	fmt.Println(numberOfResults)
 
@@ -21,17 +43,19 @@ func getResults(filename string, numberOfResults int) {
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		number, _ := strconv.Atoi(scanner.Text())
-		fmt.Println(number)
+		heap.Push(minHeap, number)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("minimum: %d\n", (*minHeap)[0])
 
 }
 
 func main() {
 
 	args := os.Args[1:]
+	minHeap := &IntHeap{}
 
 	if len(args) != 2 {
 		log.Fatal("You must supply a file to process and a number of results to display.")
@@ -39,6 +63,8 @@ func main() {
 
 	filename := args[0]
 	numberOfResults, _ := strconv.Atoi(args[1])
+	heap.Init(minHeap)
 
-	getResults(filename, numberOfResults)
+	getResults(filename, numberOfResults, minHeap)
+
 }
